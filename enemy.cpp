55e46d10d7ca1538016b1814ac80f6e7da122166
Edvard Andreassen <mitori0211@gmail.com>
2025-05-12 23:02:12 +0200

    Changed shape of enemy
    Enemy is now a quad

--- enemy.cpp
+++ enemy.cpp
@@ -6,10 +6,20 @@ Enemy::Enemy() : VisualObject()
     Vertex v1{1.0f, 0.0f, 0.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f};
     Vertex v2{0.5f, 1.0f, 0.0f,    1.0f, 0.0f, 0.7f,    0.0f, 0.0f};
     Vertex v3{0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f};
+    Vertex v4{-0.5f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f};
 
     mVertices.push_back(v1);
     mVertices.push_back(v2);
     mVertices.push_back(v3);
+    mVertices.push_back(v4);
+
+    mIndices.push_back(0);
+    mIndices.push_back(1);
+    mIndices.push_back(2);
+
+    mIndices.push_back(1);
+    mIndices.push_back(2);
+    mIndices.push_back(3);
 }
 
 void Enemy::moveEnemy()
