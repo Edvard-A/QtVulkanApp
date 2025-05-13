8a0abb997491ad68beaf51bae8c7b3669dbaa13e
Edvard Andreassen <mitori0211@gmail.com>
2025-05-13 14:03:50 +0200

    added missing position setter code
    missed including the logic in previous commit regarding new position setter

--- VisualObject.cpp
+++ VisualObject.cpp
@@ -36,6 +36,12 @@ void VisualObject::setPosition(float x, float y, float z)
 }
 
 void VisualObject::vecSetPosition(QVector3D newPosition)
+{
+    mMatrix(0, 3) = newPosition.x(); // Position in the x-axis
+    mMatrix(1, 3) = newPosition.y(); // Position in the y-axis
+    mMatrix(2, 3) = newPosition.z(); // Position in the z-axis
+}
+
 void VisualObject::moveEnemy()
 {
 
