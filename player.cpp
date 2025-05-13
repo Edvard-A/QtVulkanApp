8175aa1044f09c88f27d3f788e7c622e5266d600
Edvard Andreassen <mitori0211@gmail.com>
2025-05-13 15:39:33 +0200

    player texture
    Updated the player class to give it its own texture (UV coordinates needed to be updated as well)

--- player.cpp
+++ player.cpp
@@ -4,9 +4,10 @@
 
 Player::Player() : VisualObject()
 {
-    Vertex v1{1.0f, 0.0f, 0.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f};
-    Vertex v2{0.5f, 1.0f, 0.0f,    1.0f, 0.0f, 0.7f,    0.0f, 0.0f};
-    Vertex v3{0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f};
+    textureType = mark;
+    Vertex v1{1.0f, 0.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f};
+    Vertex v2{0.5f, 1.0f, 0.0f,    1.0f, 0.0f, 0.7f,    0.5f, 0.0f};
+    Vertex v3{0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f};
 
     mVertices.push_back(v1);
     mVertices.push_back(v2);
