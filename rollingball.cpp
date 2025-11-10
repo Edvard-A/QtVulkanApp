4f54486a7b7eefd39e56aa3d29a0a0c9dad8b4ff
Edvard <mitori0211@gmail.com>
2025-11-10 13:24:42 +0100

    Added rolling ball class

---
+++ rollingball.cpp
@@ -0,0 +1,3 @@
+#include "rollingball.h"
+
+rollingBall::rollingBall() {}
---
+++ rollingball.h
@@ -0,0 +1,11 @@
+#ifndef ROLLINGBALL_H
+#define ROLLINGBALL_H
+
+class rollingBall : public VisualObject
+{
+public:
+protected:
+private:
+};
+
+#endif // ROLLINGBALL_H
